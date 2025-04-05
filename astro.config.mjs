b0d969f60abe7c00978c7c8ae18ed278927c2316
Darko Pecevski <darkopecevski@gmail.com>
2025-04-05 21:55:54 +0200

    prepare for netlify build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [react()],
   output: 'server',
+  adapter: netlify(),
 });
